b5b4a796b19afa1bec8988695b8b4b481cb7b70d
Goni <dbtnrhs410@naver.com>
2024-01-01 20:44:38 +0900

    Account

---
+++ src/main/java/com/example/account/config/LocalRedisConfig.java
@@ -0,0 +1,33 @@
+package com.example.account.config;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import redis.embedded.RedisServer;
+
+import java.io.IOException;
+
+
+@Configuration
+public class LocalRedisConfig {
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void startRedis() throws IOException {
+        if(redisServer == null || !redisServer.isActive()){
+            redisServer = new RedisServer(redisPort);
+            redisServer.start();
+        }
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+}
